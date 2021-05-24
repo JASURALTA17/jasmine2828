b6918c6921b58a30ea66b5cdc7e7d39ab1ce9b32
suralta17 <suralta17@gmail.com>
2021-05-25 02:32:36 +0800

    Version 1 by Jasmine Wennona Suralta

---
+++ Python/Suralta.py
@@ -0,0 +1,32 @@
+#v1 = "Jasmine" 
+#v1 = v1.strip()
+
+#String Functions 
+#print(len(v1))
+#print(v1.upper())
+#print(v1.lower())
+#print(v1.replace('i', 'ea'))
+
+#v2 = input("Name: ")
+#v2 = input("Course and Section: ")
+#v2 = input("Contact Number: ")
+#v2 = input("Email Address: ")
+#print("First character: %s" %(v2[0]))
+#print("First 3 character: %s" %(v2[0:3]))
+#print("Last 3 character: %s" %(v2[len(v2)-3]))
+
+v5 = "xxxxxxxxxxxxxxxx"   
+print(v5)
+v5 = "x    _    _    x"   
+print(v5)
+v5 = "x    O    O    x"   
+print(v5)
+v5 = "x       ^      x"   
+print(v5)
+v5 = "x   \_______/  x"   
+print(v5)
+v5 = "x              x"   
+print(v5)
+v5 = "xxxxxxxxxxxxxxxx"   
+print(v5)
+v5 = v5.strip()      
